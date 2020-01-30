6a0aad81bb4f467830c9a543a870e37344275d24
Levi-Brunerclear <lwbruner91@gmail.com>
2020-01-29 23:15:15 -0700

    dynamic images

--- app.js
+++ app.js
@@ -1,57 +1,67 @@
-// function compChoice() {
-//   // let computerChoice = "";
-//   let calculation = Math.floor(Math.random() * 9);
-//   if (calculation <= 2)
-//     return "rock";
-//   else if (calculation <= 5)
-//     return "paper";
-//   else (calculation <= 8)
-//   return "scissors";
-// }
+
 
 let choice = ["rock", "paper", "scissors"]
 
-// console.log(compChoice());
+function playerRock() {
+  document.getElementById("pic-1").src = "https://images.homedepot-static.com/productImages/94af8836-0338-4802-914e-04cc71e562ad/svn/backyard-x-scapes-fake-rocks-hdd-rof-rocsb-64_1000.jpg";
+}
+function playerPaper() {
+  document.getElementById("pic-1").src = "https://epmgaa.media.clients.ellingtoncms.com/img/croppedphotos/2015/09/16/EATHTALK_PIC.jpg";
+}
+function playerScissors() {
+  document.getElementById("pic-1").src = "https://www.cchobby.com/media/catalog/product/cache/14/image/9df78eab33525d08d6e5fb8d27136e95/1/1/11196_1.jpg";
+}
 
-// function chooseRock() {
-//   let playerChoice = "rock"
-//   return playerChoice
-// }
-
-// function choosePaper() {
-//   return "paper"
-// }
-
-// function chooseScissors() {
-//   return "scissors"
-// }
+function computerRock() {
+  document.getElementById("pic-2").src = "https://images.homedepot-static.com/productImages/94af8836-0338-4802-914e-04cc71e562ad/svn/backyard-x-scapes-fake-rocks-hdd-rof-rocsb-64_1000.jpg";
+}
+function computerPaper() {
+  document.getElementById("pic-2").src = "https://epmgaa.media.clients.ellingtoncms.com/img/croppedphotos/2015/09/16/EATHTALK_PIC.jpg";
+}
+function computerScissors() {
+  document.getElementById("pic-2").src = "https://www.cchobby.com/media/catalog/product/cache/14/image/9df78eab33525d08d6e5fb8d27136e95/1/1/11196_1.jpg";
+}
 
 function play(playerChoice) {
   // rock 
-  let computerChoice = choice[Math.floor(Math.random() * choice.length)]
-  console.log("comp choice", computerChoice);
+  var computerChoice = choice[Math.floor(Math.random() * choice.length)]
 
-  if (playerChoice == computerChoice) {
-    console.log("tie")
+  if (playerChoice == "rock" && computerChoice == "rock") {
+    computerRock();
+    window.alert("It's a tie!")
   }
   if (playerChoice == "rock" && computerChoice == "scissors") {
-    console.log("win")
+    computerScissors();
+    window.alert("You won!")
   }
   if (playerChoice == "rock" && computerChoice == "paper") {
-    console.log("lose")
+    computerPaper();
+    window.alert("You lost...")
   }
   // "paper"
   if (playerChoice == "paper" && computerChoice == "rock") {
-    console.log("win")
+    computerRock();
+    window.alert("You won!")
   }
   if (playerChoice == "paper" && computerChoice == "scissors") {
-    console.log("lose")
+    computerScissors();
+    window.alert("You lost...")
+  }
+  if (playerChoice == "paper" && computerChoice == "paper") {
+    computerPaper();
+    window.alert("It's a tie!")
   }
   // "scissors" 
   if (playerChoice == "scissors" && computerChoice == "rock") {
-    console.log("lose")
+    computerRock();
+    window.alert("You lost...")
   }
   if (playerChoice == "scissors" && computerChoice == "paper") {
-    console.log("win")
+    computerPaper();
+    window.alert("You won!")
+  }
+  if (playerChoice == "scissors" && computerChoice == "scissors") {
+    computerScissors();
+    window.alert("It's a tie!")
   }
 }
